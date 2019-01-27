7d6a812bf99d31e61d573956b096eab0ee2086b2
Rune Langøy <rune.langoy@gmail.com>
2019-01-27 21:12:01 +0100

    update

--- data/index.js
+++ data/index.js
@@ -3,6 +3,28 @@
   https://github.com/yoannmoinet/nipplejs
 */
 
+/*
+var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);
+
+connection.onopen = function () {
+    connection.send('Connect ' + new Date());
+    connection.send("I123"); // Send Msg to recieve GUI update
+
+    //Debugonnection.send(rgbstr);
+    //document.getElementById("Slider1TextVal").style = "background-color:lightgreen";
+};
+
+connection.onerror = function (error) {
+    console.log('WebSocket Error ', error);
+};
+c
+onnection.onmessage = function (e) {  
+    console.log('Server: ', e.data);
+}
+
+
+*/
+
 var radius = 100;
 var sampleJoystick = {
     zone: document.getElementById('joystick_base'), 
@@ -19,29 +41,22 @@ var joystick;
 var position;
 joystick = nipplejs.create(sampleJoystick);
 
-/*
-joystick.on('move', function(evt, data) 
-{
-  console.log('Move', data);
-};
-
-joystick.on('start end', function(evt, data) {};
-
- joystick.on('dir:up plain:up dir:left plain:left dir:down plain:down dir:right plain:right', 
-  function(evt, data) {};
-
-
-.on('pressure', function(evt, data) 
-{
-  position=data;
-});
-*/
 
 joystick.on('start end', function(evt, data)
  {
   position = data; }).on('move', function(evt, data) {
   position = data;
+  
+  // <--> Send data as web-sockets
   console.log('On Move', data.distance, 'rad', data.angle.radian);
+  var xSpeed= data.distance*Math.cos(data.angle.radian)/100.0;
+  var ySpeed= data.distance*Math.sin(data.angle.radian)/100.0;
+
+  console.log('X:', xSpeed, 'Y: ', ySpeed);
+ 
+  
+ // motor1=
+
 }).on('dir:up plain:up dir:left plain:left dir:down' +
       'plain:down dir:right plain:right',
       function(evt, data) {        
